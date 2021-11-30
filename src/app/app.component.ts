b6a2f6f31a1b5711f794fdfc616086289047a98b
Vitaliy <vital344@ukr.net>
2021-11-30 23:16:58 +0200

    first commit 30.11.21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gitLearning';
+  title1 = 'gitLearning';
+  title2 = 'kot'
 }
